0737f49335556c22582778d76485e3644a63ae18
Hemachandra Reddy <130845013+Hemachandra9899@users.noreply.github.com>
2025-11-09 04:59:30 +0530

    Add production origin to CORS middleware

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 // ✅ CORS middleware – this is enough
 app.use(
     cors({
-      origin: ["http://localhost:5174", "http://localhost:5175"],
+      origin: ["http://localhost:5174", "http://localhost:5175","https://hackathon1-main.vercel.app"],
       methods: ["GET", "POST", "OPTIONS"],
       allowedHeaders: ["Content-Type", "x-api-key"],
     })
